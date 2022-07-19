cb3bbab3acddb051dcf7f89eee5130153325fdc3
Rajat Patil <rajat.patil84@gmail.com>
2022-07-20 01:52:50 +0530

    Update bot.js

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -5,7 +5,8 @@ const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 const { QnAMaker } = require('botbuilder-ai');
 const DentistScheduler = require('./dentistscheduler');
-const IntentRecognizer = require("./intentrecognizer")
+const IntentRecognizer = require('./intentrecognizer');
+
 
 class DentaBot extends ActivityHandler {
     constructor(configuration, qnaOptions) {
@@ -14,37 +15,54 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
+        this.QnAMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions);
        
         // create a DentistScheduler connector
+        this.DentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
       
         // create a IntentRecognizer connector
+        this.IntentRecognizer =  new IntentRecognizer(configuration.LuisConfiguration);
 
 
         this.onMessage(async (context, next) => {
-            // send user input to QnA Maker and collect the response in a variable
-            // don't forget to use the 'await' keyword
-          
-            // send user input to IntentRecognizer and collect the response in a variable
-            // don't forget 'await'
-                     
-            // determine which service to respond with based on the results from LUIS //
+            try{
+                // send user input to QnaMaker and collect the response in a variable
+                const qnaResults = await this.QnAMaker.getAnswers(context);
+                // send user input to IntentRecognizer and collect the response in a variable
+                // don't forget 'await'
+                const LuisResult =  await this.IntentRecognizer.executeLuisQuery(context);    
+                // top intent from LUIS
+                const topIntent =LuisResult.luisResult.prediction.topIntent;  
+                const intentScore = LuisResult.intents[topIntent].score;
 
-            // if(top intent is intentA and confidence greater than 50){
-            //  doSomething();
-            //  await context.sendActivity();
-            //  await next();
-            //  return;
-            // }
-            // else {...}
+                let replyMessage;
+                if(intentScore >= 0.65){
+                    if(topIntent === "getAvailability"){
+                        replyMessage = await this.DentistScheduler.getAvailability(this.IntentRecognizer.getTimeEntity(LuisResult));
+                    }else{
+                        replyMessage =  await this.DentistScheduler.scheduleAppointment(this.IntentRecognizer.getTimeEntity(LuisResult), this.IntentRecognizer.getDateEntity(LuisResult));
+                    }
+                }else{
+                    replyMessage = qnaResults[0].answer;
+                }
+                    
+                await context.sendActivity(MessageFactory.text(replyMessage, replyMessage));
+            }catch(e){
+                console.error(e);
+            }
              
             await next();
-    });
+        
+        });
 
         this.onMembersAdded(async (context, next) => {
         const membersAdded = context.activity.membersAdded;
         //write a custom greeting
-        const welcomeText = '';
+        const welcomeText = 'Welcome to Dental Office Virtual Assistant. I can help you to schedule your dental appointment '
+                            + 'or help you to find the next available appointment times.  '
+                            + 'You can say "schedule an appointment today at 3pm" '
+                            + 'or "are you open on weekend?"';
+
         for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
             if (membersAdded[cnt].id !== context.activity.recipient.id) {
                 await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
@@ -57,3 +75,4 @@ class DentaBot extends ActivityHandler {
 }
 
 module.exports.DentaBot = DentaBot;
+
